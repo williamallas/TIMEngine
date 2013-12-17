01530e79c13b5e593ea215cb1706ae5cf8d5b35c
williamallas <jerome.torche@epfl.ch>
2013-12-17 12:34:39 +0100

    Add Boost List

--- TIMEngine/TIMEngine/core/type.h
+++ TIMEngine/TIMEngine/core/type.h
@@ -4,6 +4,7 @@
 #include <boost/container/map.hpp>
 #include <boost/container/set.hpp>
 #include <boost/container/vector.hpp>
+#include <boost/container/list.hpp>
 #include <boost/thread.hpp>
 #include <queue>
 
